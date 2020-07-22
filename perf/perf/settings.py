6c8403a048efc5ac617410389f1e09f6e97292ad
kvenkataharsha <kvenkataharsha@gmail.com>
2020-07-23 00:29:23 +0530

    Added Merchandise i.e shop

--- perf/perf/settings.py
+++ perf/perf/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Cibil',
+    'shop',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+MEDIA_URL = '/media/'
