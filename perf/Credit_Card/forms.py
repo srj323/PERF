51371ec4f084d7a6fc7fd4a44ed0dae0fd7c9fe9
kvenkataharsha <kvenkataharsha@gmail.com>
2020-07-23 15:32:31 +0530

    added user authentication

--- perf/Credit_Card/forms.py
+++ perf/Credit_Card/forms.py
@@ -4,6 +4,7 @@ class Information(forms.Form):
     username = forms.CharField(label='Your name', max_length=100)
     firstname = forms.CharField(label='First name', max_length=100)
     lastname = forms.CharField(label='Last name', max_length=100)
+    password = forms.CharField(label='Password', max_length=100)
     email = forms.EmailField(label='Email')
     dob = forms.DateField(label='Date Of Birth')
     gender = forms.CharField(label='Gender')
